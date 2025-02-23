75bee29053235ed409f9078fb093b3ae7a1f1be3
leahkim <sxk2eh@virginia.edu>
2025-02-22 23:00:14 -0500

    eventure

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-// import backgroundImage from ".public/Eventure.png";  // Adjust relative path if needed
 
 export const Home: React.FC = () => {
     return (
         <section
-            className="relative mt-6 h-screen bg-cover bg-center"
+            className="relative mt-6  bg-cover bg-center"
             style={{ backgroundImage: `url(/Eventure.png)`, backgroundSize: 'cover', // Ensure the image covers the whole screen
-                    backgroundPosition: 'center' }}
+                backgroundPosition: 'center center',
+                width: '100vw',
+                height: '100vh' }}
         >
-            <button
-                className="overflow-hidden gap-2 self-stretch p-3 my-auto text-base leading-none bg-green-300 rounded-lg border border-solid border-zinc-300 text-neutral-100"
-            >
-                Sign in with Google
-            </button>
+            <div className="flex items-center justify-center h-full">
+                <button
+                    className="overflow-hidden gap-2 self-stretch p-3 my-auto text-base leading-none bg-green-300 rounded-lg border border-solid border-zinc-300 text-neutral-100"
+                >
+                    Sign in with Google
+                </button>
+            </div>
+
         </section>
     );
 };
+
